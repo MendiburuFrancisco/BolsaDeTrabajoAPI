4793050f9edfe290e77a4dbffa0aed6b94d2355d
Francisco <franmendi.fm@gmail.com>
2023-10-04 21:56:49 -0300

    Agregando CompanyBusiness en index

--- domain/business/index.js
+++ domain/business/index.js
@@ -6,5 +6,6 @@ module.exports = {
     MajorBusiness: require("./major.business"),
     UserBusiness: require("./user.business"),
     ApplicationBusiness: require("./application.business"),
+    CompanyBusiness: require("./company.business"),
     // UserBusiness: new BaseBusiness(  db,"usuarios"  ),
 };
