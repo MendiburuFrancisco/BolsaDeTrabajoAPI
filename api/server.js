baf89e5126f9080c73f3a32924c3b1bbbb915566
Ramacciotti Gina <ramacciottigina@gmail.com>
2023-10-09 12:12:25 -0300

    Agregando CORS para poder conectar con el front

--- api/server.js
+++ api/server.js
@@ -1,25 +1,23 @@
 const express = require('express');
-
+const cors = require('cors'); // Importa el paquete CORS
 
 class Server {
-    constructor({config,router}){
-        this._config = config;
-        this._express = express();
-        this._express.use(router);
-        
-    }
+  constructor({ config, router }) {
+    this._config = config;
+    this._express = express();
+    this._express.use(cors()); // Usa el middleware CORS aquí
+    this._express.use(router);
+  }
 
-    start(){
-        return new Promise((resolve,reject)=>{
-
-            // this._express.use(this._router);
-            const http = this._express.listen(this._config.PORT,()=>{
-                const {port} = http.address();
-                console.log(`API FRRO running on port ${port} 🚀`);
-                resolve();
-            });
-        })
-    }
+  start() {
+    return new Promise((resolve, reject) => {
+      const http = this._express.listen(this._config.PORT, () => {
+        const { port } = http.address();
+        console.log(`API FRRO running on port ${port} 🚀`);
+        resolve();
+      });
+    });
+  }
 }
 
 module.exports = Server;
